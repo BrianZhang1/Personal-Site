f6a1e07aae649b69495ac167f040d046010e1aba
BrianZhang1 <brian22ca@gmail.com>
2023-12-20 17:14:51 -0500

    Add Docker configuration for web and nginx services

--- personalsite/gunicorn.conf.py
+++ personalsite/gunicorn.conf.py
@@ -1,7 +1,3 @@
 wsgi_app = "personalsite.wsgi:application"
-loglevel = "debug"
 workers = 2
 bind = "0.0.0.0:8000"
-accesslog = errorlog = "/var/log/gunicorn/dev.log"
-capture_output = True
-daemon = True
